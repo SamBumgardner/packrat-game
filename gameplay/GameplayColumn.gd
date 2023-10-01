c5ecb4836623a811c1ceacbf4d6117b644b52d56
Noah Bumgardner <noah@playgamecafe.com>
2023-10-01 17:23:47 -0500

    Remove Leftover Column Naming, Add Backpack

--- gameplay/GameplayColumn.gd
+++ gameplay/GameplayColumn.gd
@@ -3,8 +3,4 @@ extends Control
 var column_index = 1
 
 func _ready():
-	$Header/ColumnNumber.text = str(column_index)
-
-func set_column_header_index(index):
-	column_index = index
-	$Header/ColumnNumber.text = str(column_index)
+	pass
