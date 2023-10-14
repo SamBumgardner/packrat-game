5f7e257321a065dbe7fae7c5ddb5be3cd6a496e5
Sam Bumgardner <Sambumgardner1@gmail.com>
2023-10-14 00:36:11 -0500

    pack-polish - add-item interaction between pack & region
    Lots of magic numbers abound, my apologies. Tween-writing seems to be more of an art than a science for me right now. Coming up with a good way to separate it from the hard logic of the thing would probably be very valuable.

--- gameplay/column/region/RegionContents.gd
+++ gameplay/column/region/RegionContents.gd
@@ -8,18 +8,74 @@ extends ColumnContents
 @onready var _element_display : SixElementDisplay = $Contents/SixElementDisplay
 @onready var _item_name : RichTextLabel = $Contents/ItemName
 @onready var _item_graphic : Sprite2D = $Contents/ItemGraphicControl/ItemGraphic
+@onready var _fly_to_pack_graphic : Sprite2D = $Contents/ItemGraphicControl/FlyToPackItem
+
+@onready var _tween_fly_to_pack : Tween = create_tween();
+@onready var _tween_bonk_off_pack : Tween = create_tween();
 
 var header_graphic : Texture = null
 var header_name : String = ""
+
+
 
 func _ready() -> void:
 	set_region(load("res://gameplay/column/region/region_forest.tres"))
 	set_item(_region.possible_items.pick_random())
 	set_header_properties(_region.graphic, _region.name)
 
+func _trigger_tween_fly_to_pack(target_pack : Backpack, item_accepted : bool):
+	var randomized_offset = Vector2(20, 0).rotated(randf() * PI * 2)
+	
+	_tween_fly_to_pack = create_tween()
+	_tween_fly_to_pack.set_ease(Tween.EASE_IN)
+	_tween_fly_to_pack.tween_property(_fly_to_pack_graphic, "scale", Vector2(.5, .5), .1).set_trans(Tween.TRANS_QUAD)
+	_tween_fly_to_pack.parallel().tween_property(_fly_to_pack_graphic, "position", _fly_to_pack_graphic.position + randomized_offset, .1).set_trans(Tween.TRANS_QUAD)
+	_tween_fly_to_pack.tween_property(_fly_to_pack_graphic, "global_position", target_pack.global_position, 1).set_trans(Tween.TRANS_EXPO)
+	_tween_fly_to_pack.parallel().tween_property(_fly_to_pack_graphic, "rotation", 12, 1).set_trans(Tween.TRANS_QUAD)
+	_tween_fly_to_pack.parallel().tween_property(_fly_to_pack_graphic, "modulate", Color(1,1,1,0), 1).set_trans(Tween.TRANS_EXPO)
+	
+	if item_accepted:
+		_tween_fly_to_pack.connect("finished", target_pack.react_item_added)
+	else:
+		_tween_fly_to_pack.connect("finished", target_pack.react_item_rejected)
+		_tween_fly_to_pack.connect("finished", _trigger_tween_bonk_off_pack)
+	_tween_fly_to_pack.play()
+
+func _reset_flying_tween():
+	_tween_fly_to_pack.stop()
+	_tween_bonk_off_pack.stop()
+	_reset_fly_to_pack_graphic()
+	_fly_to_pack_graphic.set_texture(_item_graphic.texture)
+	_fly_to_pack_graphic.show()
+
+func _reset_fly_to_pack_graphic():
+	_fly_to_pack_graphic.hide()
+	_fly_to_pack_graphic.rotation = 0
+	_fly_to_pack_graphic.scale = Vector2.ONE
+	_fly_to_pack_graphic.modulate.a = 1.0
+	_fly_to_pack_graphic.centered = true
+	_fly_to_pack_graphic.position = _fly_to_pack_graphic.texture.get_size() / 2
+
+func _trigger_tween_bonk_off_pack() -> void:
+	var randomized_offset = Vector2(100, 0).rotated(-PI / 6 - randf() * .66 * PI)
+	
+	_tween_bonk_off_pack = create_tween()
+	_tween_bonk_off_pack.set_ease(Tween.EASE_OUT)
+	_tween_bonk_off_pack.tween_property(_fly_to_pack_graphic, "global_position", _fly_to_pack_graphic.global_position + randomized_offset, 1).set_trans(Tween.TRANS_CIRC)
+	_tween_bonk_off_pack.parallel().tween_property(_fly_to_pack_graphic, "rotation", -6, 1).set_trans(Tween.TRANS_QUAD)
+	_tween_bonk_off_pack.parallel().tween_property(_fly_to_pack_graphic, "modulate", Color(1,1,1,1), .5).set_trans(Tween.TRANS_EXPO)
+	_tween_bonk_off_pack.set_ease(Tween.EASE_IN)
+	_tween_bonk_off_pack.parallel().tween_property(_fly_to_pack_graphic, "scale", Vector2.ZERO, .5).set_trans(Tween.TRANS_QUAD)
+	
+	_tween_bonk_off_pack.play()
+
+func _init_tween_reveal_new_item():
+	pass
+
 func next_day(column_backpack : Backpack) -> void:
 	if column_backpack != null:
-		column_backpack.add_item(_item)
+		var item_accepted : bool = column_backpack.add_item(_item)
+		fly_to_pack(column_backpack, item_accepted)
 	var randomized_item : Item = _region.possible_items.pick_random()
 	set_item(randomized_item)
 
@@ -34,3 +90,8 @@ func set_item(new_item : Item) -> void:
 	_item_name.text = new_item.name
 	_item_graphic.set_texture(new_item.graphic)
 
+func fly_to_pack(column_backpack : Backpack, item_accepted : bool) -> void:
+	_reset_flying_tween()
+	_trigger_tween_fly_to_pack(column_backpack, item_accepted)
+	$Contents/ItemGraphicControl/CPUParticles2D.restart()
+	_item_graphic.hide()
