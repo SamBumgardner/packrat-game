3a23982768e2e173a0afaabb32621c3b5774e47a
Noah Bumgardner <noah@playgamecafe.com>
2023-10-14 12:54:51 -0500

    Encapsulate Backpack Tweens, Tidy Whitespace

---
+++ gameplay/backpack/BackpackTweens.gd
@@ -0,0 +1,89 @@
+class_name BackpackTweens
+
+
+# Animate squish and slow rising action.
+static func init_tween_bounce(tween_to_init : Tween, pack : Sprite2D) -> Tween:
+	const squash_downward_grow_horizontal : float = 1.3
+	const squash_downward_shift_vertical : float = 20
+	const squash_downward_shrink_vertical : float = .9
+	const step_duration_list : Array[float] = [
+		.2,
+		.4
+	]
+
+	# Step 1 of 2
+	tween_to_init.tween_property(
+		pack,
+		"scale",
+		Vector2(
+			squash_downward_grow_horizontal,
+			squash_downward_shrink_vertical
+		),
+		step_duration_list[0]
+	).set_trans(Tween.TRANS_QUART)
+	tween_to_init.parallel().tween_property(
+		pack,
+		"position",
+		Vector2(
+			0,
+			squash_downward_shift_vertical
+		),
+		step_duration_list[0]
+	).set_trans(Tween.TRANS_QUART)
+
+	# Step 2 of 2
+	tween_to_init.tween_property(
+		pack,
+		"scale",
+		Vector2.ONE,
+		step_duration_list[1]
+	).set_trans(Tween.TRANS_LINEAR)
+	tween_to_init.parallel().tween_property(
+		pack,
+		"position",
+		Vector2.ZERO,
+		step_duration_list[1]
+	).set_trans(Tween.TRANS_LINEAR)
+
+	tween_to_init.stop()
+	tween_to_init.connect("finished", tween_to_init.stop)
+	
+	return tween_to_init
+
+
+# Animate gentle wiggle.
+static func init_tween_wiggle(tween_to_init : Tween, pack : Sprite2D) -> void:
+	const step_duration_list : Array[float] = [
+		.1,
+		.1,
+		.1
+	]
+	const wiggle_variant_clockwise : float = .05
+	const wiggle_variant_counterclockwise : float = -wiggle_variant_clockwise
+
+	# Step 1 of 3
+	tween_to_init.tween_property(
+		pack,
+		"rotation",
+		wiggle_variant_clockwise,
+		step_duration_list[0]
+	).set_trans(Tween.TRANS_CUBIC)
+
+	# Step 2 of 3
+	tween_to_init.tween_property(
+		pack,
+		"rotation",
+		wiggle_variant_counterclockwise,
+		step_duration_list[1]
+	).set_trans(Tween.TRANS_CUBIC)
+
+	# Step 3 of 3
+	tween_to_init.tween_property(
+		pack,
+		"rotation",
+		0,
+		step_duration_list[2]
+	).set_trans(Tween.TRANS_LINEAR)
+
+	tween_to_init.stop()
+	tween_to_init.connect("finished", tween_to_init.stop)
--- gameplay/item/Item.gd
+++ gameplay/item/Item.gd
@@ -2,15 +2,15 @@ extends Resource
 
 class_name Item
 
-@export var name:String = ""
-@export var graphic:Texture2D
+@export var name : String = ""
+@export var graphic : Texture2D
 
-@export var ele_nature:int = 0
-@export var ele_earth:int = 0
-@export var ele_fire:int = 0
-@export var ele_water:int = 0
-@export var ele_air:int = 0
-@export var ele_wild:int = 0
+@export var ele_nature : int = 0
+@export var ele_earth : int = 0
+@export var ele_fire : int = 0
+@export var ele_water : int = 0
+@export var ele_air : int = 0
+@export var ele_wild : int = 0
 
-@export var elements:Array[int] = [0, 0, 0, 0, 0, 0]
+@export var elements : Array[int] = [0, 0, 0, 0, 0, 0]
 # can add other qualities that all items will have here
