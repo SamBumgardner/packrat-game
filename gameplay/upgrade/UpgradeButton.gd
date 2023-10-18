b660d405c9bf64031ab8807916d70c551b4c5610
Sam Bumgardner <Sambumgardner1@gmail.com>
2023-10-17 23:16:30 -0500

    upgrade - graphics cleanup

--- gameplay/upgrade/UpgradeButton.gd
+++ gameplay/upgrade/UpgradeButton.gd
@@ -3,7 +3,7 @@ class_name UpgradeButton
 extends Button
 
 const COIN_TEXTURE = preload("res://art/silver_coin.png")
-const LEVEL_TEXTURE = preload("res://art/hammer_icon.png")
+const LEVEL_TEXTURE = preload("res://art/level_up.png")
 
 @export var upgrade_name : String = "Upgrade_Name"
 @export var cost : int = 0
@@ -26,7 +26,6 @@ func disable_with_reason(reason : UpgradeManager.RestrictedReason) -> void:
 		UpgradeManager.RestrictedReason.SHOP_LEVEL:
 			$Control.show()
 			$Control/DisabledReasonSprite.texture = LEVEL_TEXTURE
-			$Control/DisabledReasonSprite.modulate = Color.BLUE
 		_:
 			$Control.hide()
 
