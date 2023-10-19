2c875ce4a31862fe3908ba1afa5f23c11f21df8d
SamBumgardner <sambumgardner1@gmail.com>
2023-10-19 12:59:34 -0500

    fix incorrect counting logic

--- gameplay/upgrade/UpgradeButton.gd
+++ gameplay/upgrade/UpgradeButton.gd
@@ -5,6 +5,9 @@ extends Button
 const COIN_TEXTURE = preload("res://art/silver_coin.png")
 const LEVEL_TEXTURE = preload("res://art/level_up.png")
 
+const SHOP_LEVEL_TIP = "Shop Level isn't high enough yet"
+const COST_TIP = "Don't have enough coins"
+
 @export var upgrade_name : String = "Upgrade_Name"
 @export var cost : int = 0
 
@@ -23,11 +26,14 @@ func disable_with_reason(reason : UpgradeManager.RestrictedReason) -> void:
 			$Control.show()
 			$Control/DisabledReasonSprite.texture = COIN_TEXTURE
 			$Control/DisabledReasonSprite.modulate = Color.RED
+			tooltip_text = COST_TIP
 		UpgradeManager.RestrictedReason.SHOP_LEVEL:
 			$Control.show()
 			$Control/DisabledReasonSprite.texture = LEVEL_TEXTURE
+			tooltip_text = SHOP_LEVEL_TIP
 		_:
 			$Control.hide()
+			tooltip_text = ""
 
 func enable() -> void:
 	disabled = false
--- gameplay/upgrade/UpgradeManager.gd
+++ gameplay/upgrade/UpgradeManager.gd
@@ -26,8 +26,8 @@ static var change_column_customer_costs : Array[int] = [0, 30]
 static var empty_column_costs : Array[int] = [-30]
 static var remodel_costs : Array[int] = [150, 250, 350, 500]
 
-static var add_backpack_level_limits : Array[int] = [1, 2, 3, 5]
-static var add_column_level_limits : Array[int] = [1, 2, 3, 5]
+static var add_backpack_level_limits : Array[int] = [1, 2, 3, 4]
+static var add_column_level_limits : Array[int] = [0, 2, 3, 4]
 static var increase_capacity_level_limits : Array[int] = [3, 4, 5, 6]
 static var change_column_region_level_limits : Array[int] = NO_LIMIT_PER_LEVEL
 static var change_column_customer_level_limits : Array[int] = NO_LIMIT_PER_LEVEL
@@ -53,24 +53,26 @@ static var _level_limits : Array[Array] = [
 	remodel_level_limits
 ]
 static var _count_times_bought : Array[Callable] = [
-	func () : return min(add_column_costs.size() - 1, Database.backpacks_purchased),
-	func () : return min(add_column_costs.size() - 1, Database.columns_purchased),
-	func () : return min(increase_capacity_costs.size() - 1, Database.capacity_increases_purchased),
-	func () : return min(change_column_region_costs.size() - 1, Database.active_region_columns),
-	func () : return min(change_column_customer_costs.size() - 1, Database.active_customer_columns),
+	func () : return Database.backpacks_purchased,
+	func () : return Database.columns_purchased,
+	func () : return Database.capacity_increases_purchased,
+	func () : return Database.active_region_columns,
+	func () : return Database.active_customer_columns,
 	func () : return 0,
-	func () : return min(remodel_costs.size() - 1, Database.shop_level),
+	func () : return Database.shop_level,
 ]
 
 static func get_cost(upgrade_type : UpgradeType) -> int:
 	var number_of_times_bought = _count_times_bought[upgrade_type].call()
-	return _costs[upgrade_type][number_of_times_bought]
+	var max_cost_index = _costs[upgrade_type].size() - 1
+	return _costs[upgrade_type][min(number_of_times_bought, max_cost_index)]
 
 static func check_restricted(upgrade_type : UpgradeType) -> RestrictedReason:
 	var current_level = Database.shop_level
 	var number_of_times_bought = _count_times_bought[upgrade_type].call()
 	var max_number_for_level = _level_limits[upgrade_type][current_level]
-	var cost = _costs[upgrade_type][number_of_times_bought]
+	var max_cost_index = _costs[upgrade_type].size() - 1
+	var cost = _costs[upgrade_type][min(number_of_times_bought, max_cost_index)]
 	if number_of_times_bought >= max_number_for_level:
 		return RestrictedReason.SHOP_LEVEL
 	elif cost > Database.silver_coin_count:
