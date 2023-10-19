3a5d15ce112bd9ea1c195631a31490a91011a29f
SamBumgardner <sambumgardner1@gmail.com>
2023-10-18 23:49:25 -0500

    add element icons

--- gameplay/element_display/ElementAmount.gd
+++ gameplay/element_display/ElementAmount.gd
@@ -3,11 +3,13 @@ class_name ElementAmount
 extends HBoxContainer
 
 @export var color_tint : Color = Color.WHITE
+@export var element_icon : Texture = preload("res://art/wild_icon.png")
 
 var amount : int = 0
 
 func _ready():
 	$ElementIcon.modulate = color_tint
+	$ElementIcon.texture = element_icon
 
 func set_amount(new_amount) -> int:
 	amount = new_amount
