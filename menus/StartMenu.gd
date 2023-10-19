041048c442e1926d5ed6cfba7c1ea690398a26aa
Noah Bumgardner <noah@playgamecafe.com>
2023-10-18 20:35:12 -0500

    Add Background Art and Standardize Font (#17)
    * Import Backgrounds, Add Background to Victory
    
    * Add Background to Gameplay
    
    * Add Background to Start and Credits
    
    * Share Regular Font for Non-Header Non-Gameplay
    
    * Outline Text Unless Atop Semi-Transparent
    
    * Run Scenes Individually to Detect uid Updates
    
    * Add Transparent Background Behind Start Menu Text
    
    * Add Transparent Background for Credits Text
    
    * Add Transparent Background for Victory Statistics
    
    * Add Scroll Background to Upgrade Menu, Err
    
    * Start Upgrades Hidden
    
    * fix upgrade menu open/close behavior
    
    * fade-in - start menu uses dialogue scroll and fades in
    
    * allow skip fade-in
    
    * improve upgrade menu readability
    
    * add more background, extreme title screen scroll
    
    * add color to gameplay state
    
    * improve column text visibility
    
    ---------
    
    Co-authored-by: SamBumgardner <sambumgardner1@gmail.com>

--- menus/StartMenu.gd
+++ menus/StartMenu.gd
@@ -1,14 +1,28 @@
 # Scene to render menu options when the game is started.
+class_name StartMenu
+
 extends Control
 
+@onready var quit_button = (
+	$TitleContentMarginContainer/Padding8px/TitleContentRows/ButtonRows/QuitButton
+	as Button
+)
 @onready var start_button = (
-	$TitleContentContainer/ButtonsContainer/StartButton as Button
+	$TitleContentMarginContainer/Padding8px/TitleContentRows/ButtonRows/StartButton
+	as Button
 )
 
+static var first_load = true
+var visibility_tween : Tween
+
 func _ready():
-	start_button.grab_focus()
+	if !first_load:
+		force_title_appear()
+	else:
+		_initial_scroll_background()
+	first_load = false
 	if OS.get_name() == "HTML5":
-		$TitleContentContainer/ButtonsContainer/QuitButton.visible = false
+		quit_button.visible = false
 
 func _on_CreditsButton_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/CreditsMenu.tscn")
@@ -18,3 +32,32 @@ func _on_StartButton_pressed() -> void:
 
 func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
+
+func _input(event):
+	if event.is_action_pressed("gameplay_select"):
+		skip_fade_in()
+
+func _initial_scroll_background():
+	$Background.position = $Background.position + Vector2(0, -200)
+	var background_tween = $Background.create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	background_tween.tween_property($Background, "position", $Background.position + Vector2(0, 200), 4)
+
+func _on_title_fade_in_delay_timeout():
+	$TitleContentMarginContainer.modulate = Color.TRANSPARENT
+	$TitleContentMarginContainer.position = $TitleContentMarginContainer.position + Vector2(0, 20)
+	$TitleContentMarginContainer.show()
+	visibility_tween = $TitleContentMarginContainer.create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	visibility_tween.tween_property($TitleContentMarginContainer, "modulate", Color.WHITE, 2)
+	visibility_tween.parallel().tween_property($TitleContentMarginContainer, "position", $TitleContentMarginContainer.position - Vector2(0, 20), 2)
+	visibility_tween.finished.connect(start_button.grab_focus)
+
+func skip_fade_in():
+	if visibility_tween != null and visibility_tween.is_running():
+		visibility_tween.custom_step(2)
+	elif !$TitleFadeInDelay.is_stopped():
+		force_title_appear()
+
+func force_title_appear():
+	$TitleFadeInDelay.stop()
+	$TitleContentMarginContainer.show()
+	start_button.grab_focus()
