9e01dba61239b1a93c9627c66dbaeb791c89bbfd
Sam Bumgardner <Sambumgardner1@gmail.com>
2023-10-19 19:03:14 -0500

    allow pressing upgrade buttons in upgrade state

--- gameplay/states/UpgradeState.gd
+++ gameplay/states/UpgradeState.gd
@@ -27,3 +27,24 @@ func process_input(gameplay : Gameplay, event : InputEvent) -> Gameplay.State:
 			next_state = Gameplay.State.SELECT
 	
 	return next_state
+
+func handle_upgrade_selected(gameplay : Gameplay, upgrade_type : UpgradeManager.UpgradeType) -> Gameplay.State:
+	match upgrade_type:
+		UpgradeManager.UpgradeType.ADD_BACKPACK:
+			if gameplay.add_backpack():
+				return Gameplay.State.SELECT
+		UpgradeManager.UpgradeType.ADD_COLUMN:
+			gameplay.add_column()
+			return Gameplay.State.SELECT
+		UpgradeManager.UpgradeType.INCREASE_CAPACITY:
+			gameplay.enter_backpack_capacity_upgrade_mode()
+		UpgradeManager.UpgradeType.CHANGE_COLUMN_REGION:
+			gameplay.enter_column_change_mode(GlobalConstants.ColumnContents.REGION)
+		UpgradeManager.UpgradeType.CHANGE_COLUMN_CUSTOMER:
+			gameplay.enter_column_change_mode(GlobalConstants.ColumnContents.CUSTOMER)
+		UpgradeManager.UpgradeType.EMPTY_COLUMN:
+			gameplay.enter_column_change_mode(GlobalConstants.ColumnContents.NONE)
+		UpgradeManager.UpgradeType.REMODEL:
+			gameplay.start_remodel()
+			return Gameplay.State.SELECT
+	return Gameplay.State.NO_CHANGE
