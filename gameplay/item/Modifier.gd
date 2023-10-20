fb710aa144163a9c2e990b861878c2e5126b1423
Sam Bumgardner <Sambumgardner1@gmail.com>
2023-10-19 22:18:48 -0500

    move item resources

---
+++ gameplay/item/Modifier.gd
@@ -0,0 +1,15 @@
+extends Resource
+
+class_name Item
+
+@export var prefix : String = ""
+
+@export var ele_nature : int = 0
+@export var ele_earth : int = 0
+@export var ele_fire : int = 0
+@export var ele_water : int = 0
+@export var ele_air : int = 0
+@export var ele_wild : int = 0
+
+@export var elements : Array[int] = [0, 0, 0, 0, 0, 0]
+# can add other qualities that all items will have here
